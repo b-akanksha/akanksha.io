6de051d79f57100a84088a9db751906057ff0686
Akanksha C <b.akanksha6@gmail.com>
2025-03-04 12:56:05 +0530

    chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function App() {
+  return <Typography variant="subtitle">Hello World!</Typography>;
+}
+
+export default App;
